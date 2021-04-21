cc7127b4fc325ab18ad1f0fba9aa5e41b051bdf8
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-21 10:27:24 +0700

    Create 9-setter&getter.php

---
+++ 9-setter&getter.php
@@ -0,0 +1,165 @@
+<?php
+// METHOD SETTER & GETTER
+
+class Produk {
+    private $judul,
+            $penulis,
+            $penerbit,
+            $harga,
+            $diskon = 0;
+    
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+// setter getter judul
+    // setter method >> agar dapat mensetting properti dari luar >> membutuhkan parameter >> tanpa return/mengembalikan nilai
+    public function setJudul( $judul ) {
+        // melakukan validasi yg boleh ditulis hanya string
+        // if ( !is_string($judul) ) {
+        //     throw new Exception("Judul harus string!");
+        // }
+        $this->judul = $judul;
+    }
+
+    // getter method >> mencetak properti >> tanpa parameter >> harus mengembalikan nilai/return
+    public function getJudul() {
+        return $this->judul;
+    }
+
+// setter getter penulis
+    public function setPenulis( $penulis ) {
+        $this->penulis = $penulis;
+    }
+    public function getPenulis() {
+        return $this->penulis;
+    }
+
+// setter getter penerbit
+    public function setPenerbit( $penerbit ) {
+        $this->penerbit = $penerbit;
+    }
+    public function getPenerbit() {
+        return $this->penerbit;
+    }
+
+// setter getter harga
+    public function setHatga( $harga ) {
+        $this->harga = $harga;
+    }
+    // method untuk menampilkan hanya harga / visibility
+    public function getHarga() {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+// setter getter diskon
+    // method diskon
+    public function setDiskon($diskon) {
+        $this->diskon = $diskon;
+    }
+    public function getDiskon() {
+        return $this->diskon;
+    }
+
+
+    public function getPenulisPenerbit() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function cetakLengkap() {
+        $str = "{$this->judul} | {$this->getPenulisPenerbit()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+
+}
+
+class Komik extends Produk {
+
+    public $jumlahHalaman;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jumlahHalaman = 0 ) {
+        // jalankan/panggil method cunstruct dari kelas parent nya dengan overriding
+        parent::__construct( $judul, $penulis, $penerbit, $harga, $jumlahHalaman );
+
+        $this->jumlahHalaman = $jumlahHalaman;
+    }
+
+    public function cetakLengkap() { 
+        // overriding >> keyword this diganti parent:: dan bukan variabel, harus di concat
+        $str = "Komik : " . parent::cetakLengkap() . " - {$this->jumlahHalaman} halaman.";
+
+        return $str;
+    }
+
+}
+
+class Game extends Produk {
+
+    public $waktuMain;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0 ) {
+        parent::__construct( $judul, $penulis, $penerbit, $harga, $waktuMain );
+
+        $this->waktuMain = $waktuMain;
+    }
+    public function cetakLengkap() {
+        $str = "Game : " . parent::cetakLengkap() . " ~ {$this->waktuMain} jam.";
+
+        return $str;
+    }
+}
+
+class CetakInfoProduk {
+    // fungsi cetak hanya menerima parameter yang jenis nya class Produk lalu object nya
+    public function cetak(Produk $produk) {
+        $str = "{$produk->judul} | {$produk->getPenulisPenerbit()} (Rp. {$produk->harga})";
+
+        return $str;
+    }
+}
+
+$produk1 = new Komik("Naruto", "Masashi Khisimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Shingeki no Kyojin", "Hajime Isayama", "Shonen Jump", 99000, 50);
+$produk3 = new Game("Mortal Combat", "Riki Widiantoro", "Toro Groub", 234000, 77);
+$produk4 = new Produk("Dragon Ball");
+
+
+echo $produk1->cetakLengkap();
+echo "<br>";
+echo $produk2->cetakLengkap();
+echo "<br>";
+echo $produk3->cetakLengkap();
+
+echo "<hr>";
+
+$produk1->setDiskon(50);
+echo $produk1->getHarga();
+echo "<br>";
+echo $produk2->getHarga();
+
+echo "<hr>";
+
+$produk1->setJudul("judul baru"); //setter
+echo $produk1->getJudul(); //getter
+
+echo "<br>";
+
+$produk2->setDiskon(30);
+echo $produk2->getDiskon();
+echo "<br>";
+echo $produk2->getHarga();
+
+echo "<br><br>";
+
+$produk2->setPenulis("Riki Widiantoro");
+echo $produk2->getPenulis();
+
+
+// setter dan getter bisa juga menggunakan magic method
+// __set() & __get()
+
+?>
