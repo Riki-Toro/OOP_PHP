c02763716a2a7bba4ade99f89495864b5a3d9111
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-05-05 18:51:12 +0700

    Create index.php

---
+++ 15-namespace/index.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'App/init.php';
+
+$produk1 = new Komik("Naruto", "Masashi Khisimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Shingeki no Kyojin", "Hajime Isayama", "Shonen Jump", 99000, 50);
+// $produk3 = new Game("Mortal Combat", "Riki Widiantoro", "Toro Groub", 234000, 77);
+// $produk4 = new Produk("Dragon Ball");
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk( $produk1 );
+$cetakProduk->tambahProduk( $produk2 );
+echo $cetakProduk->cetak();
+
+echo "<hr>";
+
+// namespace Vendor\Namespace\SubNamespace;
+// inisialisasi class User di Service
+// new App\Produk\User();
+// echo "<br>";
+
+use App\Service\User as ServiceUser; //ngasih alias
+use App\Produk\User as ProdukUser;
+
+new ServiceUser;
+echo "<br>";
+new ProdukUser;
+
+?>
